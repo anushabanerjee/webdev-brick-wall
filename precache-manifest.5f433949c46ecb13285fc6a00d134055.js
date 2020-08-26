dd0ae2f00cc739b54b21ce628c4f5d5e78a48bbb
Anusha Banerjee <rairaianusha@gmail.com>
2020-08-26 16:09:51 +0530

    Updates

---
+++ precache-manifest.5f433949c46ecb13285fc6a00d134055.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c596dd02448e9cf9b963b0c8c854ac15",
+    "url": "/webdev-brick-wall/index.html"
+  },
+  {
+    "revision": "14e3993ef4f928cdd130",
+    "url": "/webdev-brick-wall/static/css/main.3d098d6e.chunk.css"
+  },
+  {
+    "revision": "80324c30f4f3d684b221",
+    "url": "/webdev-brick-wall/static/js/2.54423ef7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/webdev-brick-wall/static/js/2.54423ef7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14e3993ef4f928cdd130",
+    "url": "/webdev-brick-wall/static/js/main.7aca0987.chunk.js"
+  },
+  {
+    "revision": "da7b446abff9b088afb5",
+    "url": "/webdev-brick-wall/static/js/runtime-main.221575db.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/webdev-brick-wall/static/media/logo.5d5d9eef.svg"
+  }
+]);
